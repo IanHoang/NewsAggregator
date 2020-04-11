1f08fdcca3d724d4a73d9d1861bb9c3d9598715b
Tom JH Lee <jhltom@seas.upenn.edu>
2020-04-11 13:43:43 -0400

    Test parseFeed() in IndexBuilder

--- test/TestIndexBuilder.java
+++ test/TestIndexBuilder.java
@@ -1,14 +1,32 @@
 package test;
 
 import static org.junit.Assert.*;
+
+import indexing.IndexBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-/**
- * @author ericfouh
- */
-public class TestIndexBuilder
-{
-    //ToDos
+
+public class TestIndexBuilder {
+
+    List<String> feeds;
+    IndexBuilder ib;
+
+    @Before
+    public void setUp(){
+        feeds = new ArrayList<>();
+        feeds.add("http://localhost:8090/sample_rss_feed.xml");
+        ib = new IndexBuilder();
+    }
+
+    @Test
+    public void testParseFeed(){
+        Map<String, List<String>> docs = ib.parseFeed(feeds);
+        System.out.println(docs);
+    }
 
 }
